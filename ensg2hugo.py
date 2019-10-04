6f6267be0acd3e5721eae3405bd7497d92c93fc4
Chengqing Yu <54602113+Mandy-yu@users.noreply.github.com>
2019-10-04 00:04:08 -0700

    Update ensg2hugo.py

--- ensg2hugo.py
+++ ensg2hugo.py
@@ -16,6 +16,7 @@ print '"","gene_id","gene_type","logFC","AveExpr"'
 for line in fileinput.input(my_file):
     text_in_columns = re.split('\t',line)
     match = re.search('ENSG\d*',text_in_columns[1])
-    if match.grou() in Lookup_geneID:
-        text_in_columns[4]=re.sub('\n','',text_in_columns[4].rstrip());
-        print text_in_columns[0] + ",\"" + Lookup_geneID[text_in_columns[1]] + "\"," + text_in_columns[2] + "," + text_in_columns[3] + "," + text_in_columns[3] + "," + text_in_columns[4]
+    if match:
+       if match.grou() in Lookup_geneID:
+          text_in_columns[4]=re.sub('\n','',text_in_columns[4].rstrip());
+          print text_in_columns[0] + ",\"" + Lookup_geneID[text_in_columns[1]] + "\"," + text_in_columns[2] + "," + text_in_columns[3] + "," + text_in_columns[3] + "," + text_in_columns[4]
