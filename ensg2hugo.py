68cf37a8cbc0f593855a672ae96e494a2d78c31b
Chengqing Yu <54602113+Mandy-yu@users.noreply.github.com>
2019-10-04 00:13:53 -0700

    Update ensg2hugo.py

--- ensg2hugo.py
+++ ensg2hugo.py
@@ -14,7 +14,7 @@ for line in fileinput.input(['Homo_sapiens.GRCh37.75.gtf']):
              Lookup_geneID[gene_id_matches[0]] = gene_name_matches[0]
 print '"","gene_id","gene_type","logFC","AveExpr"'
 for line in fileinput.input(my_file):
-    text_in_columns = re.split('\t',line)
+    text_in_columns = re.split(',',line)
     match = re.search('ENSG\d*',text_in_columns[1])
     if match:
        if match.grou() in Lookup_geneID:
